04c475e7b0c79b910bc8618884abe26d54c60714
Zack Devine <2781577+zackdevine@users.noreply.github.com>
2019-01-27 03:14:36 -0500

    add digibot

---
+++ digibot.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+memes = commands.Bot(command_prefix='!')
+
+@memes.event
+async def on_ready():
+    await memes.change_presence(activity=discord.Game(name="absolutely nothing"))
+    
+memes.run(token)
