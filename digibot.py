7ed0dbd7e4507b1147d2e6e0be1ed2767e80bc98
Tarek1337 <Tarek1337@users.noreply.github.com>
2019-02-15 22:25:07 +0100

    Added a command that does nothing.

--- digibot.py
+++ digibot.py
@@ -6,5 +6,9 @@ memes = commands.Bot(command_prefix='!')
 @memes.event
 async def on_ready():
     await memes.change_presence(activity=discord.Game(name="absolutely nothing"))
+
+@memes.command()
+async def nothing(ctx):
+    pass
     
 memes.run(token)
